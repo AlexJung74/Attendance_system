908e1e427967a8660db76246c4856c014eef8bd2
AlexJung74 <161559971+AlexJung74@users.noreply.github.com>
2024-09-29 17:49:11 +1300

    Production initial data

--- settings/production.py
+++ settings/production.py
@@ -8,9 +8,7 @@ DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # 추가적인 프로덕션 설정 (보안, 로깅 등)
