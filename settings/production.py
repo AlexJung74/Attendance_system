c8665ae75237c3ea9e520f013e794e8603870203
AlexJung74 <161559971+AlexJung74@users.noreply.github.com>
2024-11-17 00:12:26 +1300

    Change DB Path3

--- settings/production.py
+++ settings/production.py
@@ -11,6 +11,21 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
 
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+}
+
 # 추가적인 프로덕션 설정 (보안, 로깅 등)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
