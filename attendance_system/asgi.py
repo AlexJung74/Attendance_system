b86df7d7498d863a5f50ee55cda4143d7a8a2381
AlexJung74 <161559971+AlexJung74@users.noreply.github.com>
2024-11-17 11:22:52 +1300

    Backend Create amend

--- attendance_system/asgi.py
+++ attendance_system/asgi.py
@@ -8,12 +8,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+import logging
 from django.core.asgi import get_asgi_application
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
-application = get_asgi_application()
+try:
+    application = get_asgi_application()
+    logger.debug("ASGI application loaded successfully.")
+except Exception as e:
+    logger.error("Error loading ASGI application: %s", e)
+    raise e
 
 # Vercel 호환성을 위한 handler 변수
 handler = application
