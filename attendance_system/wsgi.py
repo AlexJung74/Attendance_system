e3a6cf6fef7ef73b8e77d0e44bfb5148a8154d87
AlexJung74 <161559971+AlexJung74@users.noreply.github.com>
2024-11-17 06:31:35 +1300

    Backend Create amend

--- attendance_system/wsgi.py
+++ attendance_system/wsgi.py
@@ -11,11 +11,27 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+import logging
+
+
+# 로깅 설정
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# 환경 변수 로깅
+logger.debug("Initializing WSGI application...")
+logger.debug("DJANGO_SETTINGS_MODULE: %s", os.environ.get('DJANGO_SETTINGS_MODULE'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
-# WSGI 애플리케이션 정의
-application = get_wsgi_application()
+try:
+    # WSGI 애플리케이션 생성
+    application = get_wsgi_application()
+    logger.debug("WSGI application initialized successfully.")
+except Exception as e:
+    logger.error("Error initializing WSGI application: %s", e)
+    raise e
 
-# Vercel 호환성을 위한 handler 변수 추가
+# Vercel 호환성을 위한 handler 추가
 handler = application
+logger.debug("Handler assigned successfully.")
