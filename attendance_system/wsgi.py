c59d0f7002f7418e610e85cdf598110d760abd5f
AlexJung74 <161559971+AlexJung74@users.noreply.github.com>
2024-11-17 04:47:57 +1300

    Backend Create amend

--- attendance_system/wsgi.py
+++ attendance_system/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
+# 기존 WSGI 애플리케이션 정의
 application = get_wsgi_application()
+
+# Vercel 호환성을 위한 app 변수 추가
+app = application
