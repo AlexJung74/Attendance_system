494dcf5565e002f6accec8d6212bd5e858a01da4
AlexJung74 <161559971+AlexJung74@users.noreply.github.com>
2024-11-17 00:10:26 +1300

    Change DB Path2

--- settings/production.py
+++ settings/production.py
@@ -8,7 +8,7 @@ DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
 
 # 추가적인 프로덕션 설정 (보안, 로깅 등)
