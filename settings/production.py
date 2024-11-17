2cef7c9d5b3b23b8b1282af7ba9d7230b143000f
AlexJung74 <zend74@gmail.com>
2024-11-17 22:22:27 +1300

    Backend build to Heroku

--- settings/production.py
+++ settings/production.py
@@ -1,12 +1,14 @@
+# settings/production.py
+
 from .base import *
 import dj_database_url
 
 DEBUG = False  # 프로덕션 환경에서는 False로 설정
 
 ALLOWED_HOSTS = [
-    'attendance-system-backend-theta.vercel.app',
+    'attendance-backend-40491d7871de.herokuapp.com',
+    'attendance-system-theta.vercel.app',
     'attendance-system-frontend-seven.vercel.app',
-    'attendance-backend-40491d7871de.herokuapp.com,'
     'localhost', '127.0.0.1'
 ]
 
