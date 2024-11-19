5587e9eb21e9a4388941d047d50393003224b5c6
AlexJung74 <zend74@gmail.com>
2024-11-19 15:59:42 +1300

    Connect between backend and frontend

--- react_frontend/vite.config.js
+++ react_frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: '/', // 기본 경로로 설정
+    base: '/', // Vercel 배포 시 기본 경로를 루트로 설정
 
     plugins: [react()],
     server: {
