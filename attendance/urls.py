781eccfd74489c7a8c297e658247b6eb4f66e289
AlexJung74 <zend74@gmail.com>
2024-11-27 10:40:44 +1300

    tt

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,7 +2,6 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.viewsets import options_request_handler
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views.admin_views import (
@@ -42,7 +41,6 @@ urlpatterns = [
     # 관리자 대시보드 (템플릿 및 API)
     path('api/admin/dashboard/', AttendanceCheckAPIView.as_view(), name='admin_attendance_api'),
     path('dashboard/', admin_dashboard, name='admin_dashboard'),
-    path('api/options/', options_request_handler, name='options-request'),
 
 
     # 출석 관리
